4aabce5265e05a392ec23a4c485c8506eec8eeba
GrayR00t <ns28@iitbbs.ac.in>
2022-03-04 10:37:41 +0530

    first smart contracr

---
+++ hello_world.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.1;
+
+contract MyContract {
+    string public myString = 'hello world';
+}
+
+
+//pragma solidity ^0.8.1, the pragma keyword is for compiler to enable certain feature or check certain things.
+// contract MyContract is actual beginning of smart contract like any other language.
+//string public myString is public storage variable to store string data type .
